ae1c8a2fb24e811c8d846567a6b7e58133cc48b5
kwinkich <vankord21@gmail.com>
2023-05-17 16:54:31 +0300

    update code and fix bug

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ let button = document.querySelector('.random-btn');
 let span = document.querySelector('.random-span');
 let firstInput = document.querySelector('.first-value');
 let secondInput = document.querySelector('.second-value');
+let paragraph = document.querySelector('.random-num');
 let number;
 
 function randomNumber() {
-  button.addEventListener('click', () => {
-    const first = parseInt(firstInput.value);
-    const second = parseInt(secondInput.value);
+	button.addEventListener('click', () => {
+		const first = parseInt(firstInput.value);
+		const second = parseInt(secondInput.value);
 
-    if (isNaN(first) || isNaN(second)) {
-      span.innerHTML = 'Заполните поля!';
-    } else if (first >= second) {
-      span.innerHTML = 'Первое число должно быть меньше второго числа!';
-    } else {
-      number = Math.floor(Math.random() * (second - first + 1) + first);
-      console.log(number);
-      span.innerHTML = `${number}`;
-    }
-  });
+		if (isNaN(first) || isNaN(second)) {
+			span.innerHTML = 'Заполните поля!';
+		} else if (first >= second) {
+			span.innerHTML = 'Первое число должно быть меньше второго числа!';
+		} else if (second == first) {
+			span.innerHTML = 'Второе число должно быть больше первого!';
+		} else if (second < 0 || first < 0) {
+			span.innerHTML = 'Числа не могут быть отрицательными!';
+		} else {
+			number = Math.floor(Math.random() * (second - first + 1) + first);
+			console.log(number);
+			paragraph.innerHTML = 'Ваше число:';
+			span.innerHTML = `${number}`;
+		}
+
+	});
 }
 
 randomNumber();
